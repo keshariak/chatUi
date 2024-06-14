5791c119224ac737b56d68c3e52e7192bdb28718
keshariak <aryankeshari2003@gmail.com>
2024-06-14 20:22:08 +0530

    time in msg

--- src/Component/MessegeReceive.tsx
+++ src/Component/MessegeReceive.tsx
@@ -4,6 +4,12 @@ function MessegeReceive() {
   return (
     <div className=' w-fit max-w-lg p-3 py-2 mt-2 rounded-3xl mr-auto bg-[#FAFAFA]'>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex accusamus qui labore?</p>
+    <div className='flex'>
+    <p className='text-[12px]  ml-auto'>12:12</p>
+
+    </div>
+    
+
 </div>
   )
 }
--- src/Component/MessegeSend.tsx
+++ src/Component/MessegeSend.tsx
@@ -4,6 +4,11 @@ function MessegeSend() {
   return (
     <div className=' w-fit max-w-[60%] p-3 py-2 my-1 rounded-3xl ml-auto bg-[#DCF7C5]'>
         <p>Lorem ipsum dolor sit amet consectetur elit. Examet consectetur adipisicing elit. Exsasasasas accusamus qui labore?</p>
+        <div className=' flex '>
+        <p className='text-[12px]  ml-auto'>12:12</p>
+
+        </div>
+       
     </div>
   )
 }
